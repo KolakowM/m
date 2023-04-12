97944954dd2f59df831aa0cb9e7adafc8fba3318
KolakowM <drugikolakowski@gmail.com>
2023-04-12 18:37:56 +0200

    Dodanie fajnego pola do edycji opisu tekstu

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 2 }
+    
+
+    has_rich_text :body
     has_one_attached :cover_image
 
-    enum item_type: %i[kot pies Inne]
+    enum item_type: %i[kot pies inne]
 end
